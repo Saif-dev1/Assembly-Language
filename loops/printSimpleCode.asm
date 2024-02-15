0c4e363fb0cd4c97b773fa038beffd51af032c0e
Saifullah Latki <116554950+Saif-dev1@users.noreply.github.com>
2024-02-15 11:33:17 +0500

    printSimpleCode.asm
    To print the hello 5 times

---
+++ loops/printSimpleCode.asm
@@ -0,0 +1,33 @@
+.model small
+.stack 100h
+.data
+        var1 db 'Hello $'
+        count db '1'
+.code
+main proc
+
+ mov ax,@data
+ mov ds,ax
+ mov cx,5
+
+ lab1:
+        mov dx,offset var1 ;to use variables
+        mov ah,9
+        int 21h
+        mov ah,2
+
+        mov dl,count ;to increasement 
+        int 21h
+        add count,1
+
+        mov ah,2
+        mov dl,10
+        int 21h
+        mov dl,13
+        int 21h
+  loop lab1
+
+ mov ah,4ch
+ int 21h
+ main endp
+ end main
