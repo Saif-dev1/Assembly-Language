a8966e1444567c1c1a4393f484e58d3c0656e06c
Saifullah Latki <116554950+Saif-dev1@users.noreply.github.com>
2024-02-22 11:33:22 +0500

    printName.asm
    print name in loop 10 times

---
+++ loops/printName.asm
@@ -0,0 +1,34 @@
+.model small
+.stack 100h
+.data
+        str1 db 'Saifullah $'
+.code
+main proc
+
+mov ax,@data
+mov ds,ax
+mov cx,10
+
+lab1:
+
+        mov dx,offset str1
+        mov ah,9
+        int 21h
+        mov ah,2
+
+        mov dl,10
+        int 21h
+        mov dl,13
+        int 21h
+
+
+
+
+loop lab1
+
+mov ah,4ch
+int 21h
+
+Main endp
+end main
+        
