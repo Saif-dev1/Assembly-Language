69aca433cf36a77fc076aa28678d9408721db17a
Saifullah Latki <116554950+Saif-dev1@users.noreply.github.com>
2024-03-06 17:23:00 +0500

    alphabetPattern.asm
    printing english alphabet in nested loop

---
+++ loops/Nested Loops/alphabetPattern.asm
@@ -0,0 +1,41 @@
+.model small
+.stack 100h
+.data
+    i dw 5
+    j dw 1
+    k db 'A'
+.code
+main proc
+mov ax,@data
+mov ds,ax
+
+mov cx,i
+loop1:
+
+PUSH cx
+    mov cx,j
+
+    loop2:
+
+    mov ah,2
+    mov dl,k
+    int 21h
+    inc k
+
+    loop loop2
+mov k,'A'
+POP cx
+inc j
+
+mov dl,10
+int 21h
+mov dl,13
+int 21h
+
+
+loop loop1
+
+mov ah,4ch
+int 21h
+main endp
+end main
